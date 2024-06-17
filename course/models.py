35de9d1257417e84204403464c300dba29967863
Esther-Shiamagovi <shiamagoviesther@gmail.com>
2024-06-17 20:11:14 +0300

    added a django project with app teacher,studet,course and class

---
+++ course/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Course(models.Model):
+   course_name= models.CharField(max_length=20)
+   course_code = models.IntegerField()
+   teacher = models.CharField(max_length=30)
+   number_of_topics = models.IntegerField()
+   duration_of_course= models.DateField()
+   number_of_students= models.IntegerField()
+   description_of_course= models.TextField()
+   fees_required= models.BooleanField(default= False)
+   from django.db import models
+   
+def __str__(self):
+       return f"{self.course_name} {self.course_code}"
+# Create your models here.
